72e79e3876bf3bc2e20e89c20f5d88da0637b230
sameersharma1 <prosameer1111@gmail.com>
2017-03-14 20:10:10 +0530

    Add files via upload

---
+++ Bit Operations/2.c
@@ -0,0 +1,41 @@
+/*Program to evaluate number of set bits*/
+#include <stdio.h>
+
+void show(int);
+int count(int);
+
+int main()
+{
+	int num;
+
+	printf("Enter any number : ");
+	scanf("%d",&num);
+
+	show(num);	
+	printf("Total %d bits are set...!!\n",count(num));
+	return 0;
+}
+
+void show(int n)
+{
+	int i;
+
+	for (i=1<<15; i>0; i=i/2)
+		(n & i) ? printf("1") : printf("0");
+
+	printf("\n");
+}
+
+int count(int n)
+{
+	int s=0;
+	
+	while(n)
+	{
+		if(n & 1)
+			s++;
+		n = n>>1;
+	}
+
+	return s;
+}
